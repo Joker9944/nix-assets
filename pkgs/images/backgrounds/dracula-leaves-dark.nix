c5a0c07c74f74b18d4b72ffe6fc9d90aa803f62f
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-08-20 17:57:24 +0200

    dracula-leaves-dark: add background

---
+++ pkgs/images/backgrounds/dracula-leaves-dark.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  utility,
+  ...
+}:
+utility.custom.mkImageDerivationSet {
+  inherit pkgs;
+  pname = "dracula-leaves-dark";
+  type = "backgrounds";
+} {
+  definitions = [
+    {resolution = "1920x1080";}
+    {resolution = "2560x1440";}
+    {resolution = "3440x1400";}
+  ];
+}
