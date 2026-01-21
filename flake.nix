e826c30e77b7ab1811325072fc5324fa4a35632f
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-01-21 14:57:52 +0100

    cspell: update config

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "nix-assets";
 
   inputs = {
+    self.submodules = true;
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils/main"; # cSpell:ignore numtide
     pre-commit-hooks = {
