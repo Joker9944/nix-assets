100823738433146d710cf3a92ce7e942b6a1d211
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-04-08 00:27:52 +0200

    aerial-photo-of-brown-mountains: init background

--- pkgs/aerial-photo-of-brown-mountains.nix
+++ pkgs/aerial-photo-of-brown-mountains.nix
@@ -1,13 +1,13 @@
 { lib, imageTools, ... }:
 imageTools.buildProvidedImageModule {
-  pname = "starry-night-sky-over-a-calm-lake-reflecting-stars";
+  pname = "aerial-photo-of-brown-mountains";
   version = "1.0.0";
 
   type = "backgrounds";
   filetype = "jpg";
 
   meta = {
-    homepage = "https://unsplash.com/photos/starry-night-sky-over-a-calm-lake-reflecting-stars-Wfmjg9oySJY";
+    homepage = "https://unsplash.com/photos/aerial-photo-of-brown-moutains-JgOeRuGD_Y4";
     license = lib.licenses.unfree;
   };
 }
